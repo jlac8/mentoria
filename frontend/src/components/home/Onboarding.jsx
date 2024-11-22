8f15a518c4d7560c19ea3dd15bb94fd81ee1368c
Juan <jlalvac@uni.pe>
2024-11-22 17:55:44 -0500

    style: :lipstick: update homepage style

--- frontend/src/components/home/Onboarding.jsx
+++ frontend/src/components/home/Onboarding.jsx
@@ -5,16 +5,18 @@ const Onboarding = () => {
   const navigate = useNavigate();
 
   return (
-    <div className="text-center text-white px-4 font-nunito">
+    <div className="text-center text-white px-4 font-nunito max-w-md mx-auto">
       <div className="text-6xl font-sans mb-4">🧙‍♂️</div>
 
       <h1 className="text-6xl font-bold mb-6">MentorIA</h1>
+      <p className="text-xl mb-6 font-bold">¡Saludos, joven aventurero!</p>
       <p className="text-xl mb-6">
-        ¡Bienvenido a Mentoria! Estoy aquí para guiarte en tu camino de
-        aprendizaje.
+        Soy tu guía místico en este viaje hacia la realización de tus más
+        profundos deseos y el máximo esplendor de tu potencial. ¿Estás listo
+        para adentrarte en este sendero mágico?
       </p>
       <Button
-        text="Comenzar"
+        text="¡Sí, estoy listo!"
         onClick={() => {
           navigate("/chat");
         }}
